773e73983c28e5288874f7ca675bc85474692444
mbogdanoff <mbogdanov@mail.com>
2015-03-13 22:00:04 +0200

    testing

--- WebContent/scripts/testGet2.js
+++ WebContent/scripts/testGet2.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
     function processResponse(response){
        console.log(JSON.stringify(response))
         //$.each(response,function(){
-        if(response.city.indexOf($("#inputpictureid").val()) >- 1) {
+        if(response.city.indexOf($("#inputpictureid").val()) >- 1
+            && response.season.indexOf($("#seasoninput").val()) >- 1 ) {
            console.log("entered a city")
             addPictureInfo(response)
         }
@@ -12,14 +13,14 @@ $(document).ready(function() {
     }
 
     function addPictureInfo(response){
-        var ImageElement = $("<img src="+ JSON.stringify(response.imgsource) +">")
-        $("#imageHolder").append(ImageElement);
-        var placeElement = $("<p>"+ JSON.stringify(response.city) +"</p>"
-        $("#imageHolder").append(placeElement);
-        var seasonElement = $("<p>"+ JSON.stringify(response.season) +"</p>")
-        $("#imageHolder").append(seasonElement);
-        var objectNameElement = $("<p>"+ JSON.stringify(response.objectname) +"</p>")
-        $("#imageHolder").append(objectNameElement);
+        var ImageElement = $("<img src="+ JSON.stringify(response.imgsource) +">");
+        $("#content").append(ImageElement);
+        var placeElement = $("<p>"+ JSON.stringify(response.city) +"</p>");
+        $("#content").append(placeElement);
+        var seasonElement = $("<p>"+ JSON.stringify(response.season) +"</p>");
+        $("#content").append(seasonElement);
+        var objectNameElement = $("<p>"+ JSON.stringify(response.objectname) +"</p>");
+        $("#content").append(objectNameElement);
     }
 
     $("#getBtn  ").click(function(){
