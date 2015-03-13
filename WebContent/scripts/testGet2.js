b3f58b56d874d7207c11799c78ad3670b6e2adfd
mbogdanoff <mbogdanov@mail.com>
2015-03-13 20:53:47 +0200

    Added picture info

--- WebContent/scripts/testGet2.js
+++ WebContent/scripts/testGet2.js
@@ -2,23 +2,28 @@ $(document).ready(function() {
     "use strict";
 
     function processResponse(response){
-      // console.log(JSON.stringify(response.city))
+       console.log(JSON.stringify(response))
         //$.each(response,function(){
         if(response.city.indexOf($("#inputpictureid").val()) >- 1) {
-            console.log(JSON.stringify(response.season));
-            console.log(JSON.stringify(response.objectname));
-            console.log(JSON.stringify(response.imgsource));
-            addPictureInfo(JSON.stringify(response.imgsource))
+           console.log("entered a city")
+            addPictureInfo(response)
         }
        // });
     }
 
-    function addPictureInfo(pictureURL){
-        var newImageElement = $("<img src="+ pictureURL +">")
-        $("#imageHolder").append(newImageElement);
+    function addPictureInfo(response){
+        var ImageElement = $("<img src="+ JSON.stringify(response.imgsource) +">")
+        $("#imageHolder").append(ImageElement);
+        var placeElement = $("<p>"+ JSON.stringify(response.city) +"</p>"
+        $("#imageHolder").append(placeElement);
+        var seasonElement = $("<p>"+ JSON.stringify(response.season) +"</p>")
+        $("#imageHolder").append(seasonElement);
+        var objectNameElement = $("<p>"+ JSON.stringify(response.objectname) +"</p>")
+        $("#imageHolder").append(objectNameElement);
     }
 
-    $("#getBtnId").click(function(){
+    $("#getBtn  ").click(function(){
+        console.log("button clicked")
         $.ajax("http://private-6ba54-worldview.apiary-mock.com/photos/"+$("#inputpictureid").val(), {
             method: "GET"
         }).then(processResponse);
