1bff4660409be568fd74fbf378e725af6cf1dc97
mbogdanoff <mbogdanov@mail.com>
2015-03-13 20:20:27 +0200

    Showing the picture on the web page

--- WebContent/scripts/testGet2.js
+++ WebContent/scripts/testGet2.js
@@ -2,13 +2,20 @@ $(document).ready(function() {
     "use strict";
 
     function processResponse(response){
-       console.log(JSON.stringify(response))
-        $.each(response,function(){
-            if(response.id==1){
-                console.log(JSON.stringify(response.city));
-            }
-            //i++;
-        });
+      // console.log(JSON.stringify(response.city))
+        //$.each(response,function(){
+        if(response.city.indexOf($("#inputpictureid").val()) >- 1) {
+            console.log(JSON.stringify(response.season));
+            console.log(JSON.stringify(response.objectname));
+            console.log(JSON.stringify(response.imgsource));
+            addPictureInfo(JSON.stringify(response.imgsource))
+        }
+       // });
+    }
+
+    function addPictureInfo(pictureURL){
+        var newImageElement = $("<img src="+ pictureURL +">")
+        $("#imageHolder").append(newImageElement);
     }
 
     $("#getBtnId").click(function(){
