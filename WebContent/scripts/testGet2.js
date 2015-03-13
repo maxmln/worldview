b1d65634921836e222ecc979c0ede85360348dd9
mbogdanoff <mbogdanov@mail.com>
2015-03-13 19:23:13 +0200

    adding some functionality

--- WebContent/scripts/testGet2.js
+++ WebContent/scripts/testGet2.js
@@ -1,1 +1,20 @@
-alert("hello");
+$(document).ready(function() {
+    "use strict";
+
+    function processResponse(response){
+       console.log(JSON.stringify(response))
+        $.each(response,function(){
+            if(response.id==1){
+                console.log(JSON.stringify(response.city));
+            }
+            //i++;
+        });
+    }
+
+    $("#getBtnId").click(function(){
+        $.ajax("http://private-6ba54-worldview.apiary-mock.com/photos/"+$("#inputpictureid").val(), {
+            method: "GET"
+        }).then(processResponse);
+    })
+
+});
